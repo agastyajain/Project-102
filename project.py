895de12b6f48af1e7135cdf4ca7ee6ea017884fb
agastyajain <71954407+agastyajain@users.noreply.github.com>
2021-07-05 17:24:15 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 # this project automates our message sending on whatsapp
 import pywhatkit
-pywhatkit.sendwhatmsg('+91 981191851','Hello',17,37)
+pywhatkit.sendwhatmsg('+91 981191851','Hello',17,26)
 #1st argument takes the mobile number
 #2nd argument takes the message
 #3rd and 4th arguments take the time at which the message to be sent
